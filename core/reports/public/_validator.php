9b0d149263638f5ec165c8713cb84182cb1218f0
JVillator0 <Javier99.villatoro@gmail.com>
2018-11-01 21:26:03 -0600

    cambiando nombre de readme txt y aplicando validator en los reportes del public

---
+++ core/reports/public/_validator.php
@@ -0,0 +1,14 @@
+<?php 
+//setea el metodo que pasara antes de ocurrir errores
+set_error_handler("exceptions_error_handler");
+//si hay errores se ejecuta, obteniendo las variables de los errores
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    //si no hay ningun error no pasa nada, le retornar nada y la funcion termina en esta linea
+    if(error_reporting() == 0) {  return; }
+    if (error_reporting() & $severity) {
+        //le decimos a donde redireccionara si algo no funciona
+        $ruta = "../../../pages/dashboard/index.php";
+        header("location:" . $ruta);
+    }
+}
+?>
